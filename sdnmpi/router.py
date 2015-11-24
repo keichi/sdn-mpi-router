fa4eb3206cf42bfa26f556d88e132d67ed41e57e
Keichi Takahashi <keichi.t@me.com>
2015-11-24 16:27:21 +0900

    Implement routing

--- sdnmpi/router.py
+++ sdnmpi/router.py
@@ -1,15 +1,17 @@
-import struct
+#  import struct
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER, set_ev_cls
 from ryu.controller.event import EventBase, EventRequestBase, EventReplyBase
 from ryu.ofproto import ofproto_v1_0
 from ryu.lib.mac import haddr_to_bin, BROADCAST_STR
 from ryu.lib.packet import packet, ethernet, ether_types
 
 from util.switch_fdb import SwitchFDB
-from process import RankResolutionRequest, ProcessManager
+from topology import FindRouteRequest
+#  from process import RankResolutionRequest, ProcessManager
+from process import ProcessManager
 
 
 class EventFDBUpdate(EventBase):
@@ -42,12 +44,17 @@ class Router(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(Router, self).__init__(*args, **kwargs)
         self.fdb = SwitchFDB()
+        self.dps = {}
 
-    def _add_flow(self, datapath, in_port, dst, actions):
+    def _add_flow(self, datapath, src, dst, out_port):
         ofproto = datapath.ofproto
 
         match = datapath.ofproto_parser.OFPMatch(
-            in_port=in_port, dl_dst=haddr_to_bin(dst))
+            dl_src=haddr_to_bin(src), dl_dst=haddr_to_bin(dst))
+
+        actions = [
+            datapath.ofproto_parser.OFPActionOutput(out_port),
+        ]
 
         mod = datapath.ofproto_parser.OFPFlowMod(
             datapath=datapath, match=match, cookie=0,
@@ -60,6 +67,20 @@ class Router(app_manager.RyuApp):
     def _current_fdb_request_handler(self, req):
         reply = CurrentFDBReply(req.src, self.fdb)
         self.reply_to_request(req, reply)
+
+    @set_ev_cls(ofp_event.EventOFPStateChange,
+                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def state_change_handler(self, ev):
+        dp = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if dp.id is None:
+                return
+            self.dps[dp.id] = dp
+        elif ev.state == DEAD_DISPATCHER:
+            if dp.id is None:
+                return
+            if dp.id in self.dps:
+                del self.dps[dp.id]
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -76,57 +97,65 @@ class Router(app_manager.RyuApp):
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
         # ignore broadcast packet (broadcasts are handled by TopologyManager)
-        if eth.dst == BROADCAST_STR:
+        if dst == BROADCAST_STR:
+            return
+        # ignore IPv6 multicast packets
+        if dst.startswith("33:33"):
             return
 
         dpid = datapath.id
 
         self.logger.info("Packet in %s %s %s %s", dpid, src, dst, msg.in_port)
 
-        # learn a mac address to avoid FLOOD next time.
-        self.fdb.update(dpid, src, msg.in_port)
-        self.send_event_to_observers(EventFDBUpdate(dpid, src, msg.in_port))
+        print "Finding route from %s to %s" % (src, dst)
+        fdb = self.send_request(FindRouteRequest(src, dst)).fdb
+        print "Found route is: %s" % fdb
+        # Install rules to all datapaths in path
+        if fdb:
+            for (dpid, out_port) in fdb:
+                if dpid in self.dps:
+                    datapath = self.dps[dpid]
+                    self._add_flow(datapath, src, dst, out_port)
 
-        # check if we know the destionation port; if not we flood
-        out_port = self.fdb.get_port(dpid, dst)
-        if out_port is None:
-            out_port = ofproto.OFPP_FLOOD
+        # if dst.startswith("02:00"):
+        #     bin_dst = haddr_to_bin(dst)
+        #     src_rank = struct.unpack("<h", bin_dst[2:4])[0]
+        #     dst_rank = struct.unpack("<h", bin_dst[4:6])[0]
 
-        actions = [
-            datapath.ofproto_parser.OFPActionOutput(out_port),
-        ]
-        if dst.startswith("02:00"):
-            bin_dst = haddr_to_bin(dst)
-            src_rank = struct.unpack("<h", bin_dst[2:4])[0]
-            dst_rank = struct.unpack("<h", bin_dst[4:6])[0]
+        #     self.logger.info("SDNMPI communication from rank %s to rank %s",
+        #                      src_rank, dst_rank)
 
-            self.logger.info("SDNMPI communication from rank %s to rank %s",
-                             src_rank, dst_rank)
-
-            reply = self.send_request(RankResolutionRequest(dst_rank))
-            dst_mac = reply.mac
-            out_port = self.fdb.get_port(dpid, dst_mac)
-            if out_port is not None:
-                actions = [
-                    datapath.ofproto_parser.OFPActionSetDlDst(
-                        haddr_to_bin(dst_mac)
-                    ),
-                    datapath.ofproto_parser.OFPActionOutput(out_port),
-                ]
-            else:
-                out_port = ofproto.OFPP_FLOOD
-                actions = [
-                    datapath.ofproto_parser.OFPActionOutput(out_port),
-                ]
-
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            self._add_flow(datapath, msg.in_port, dst, actions)
+        #     reply = self.send_request(RankResolutionRequest(dst_rank))
+        #     dst_mac = reply.mac
+        #     out_port = self.fdb.get_port(dpid, dst_mac)
+        #     if out_port is not None:
+        #         actions = [
+        #             datapath.ofproto_parser.OFPActionSetDlDst(
+        #                 haddr_to_bin(dst_mac)
+        #             ),
+        #             datapath.ofproto_parser.OFPActionOutput(out_port),
+        #         ]
+        #     else:
+        #         out_port = ofproto.OFPP_FLOOD
+        #         actions = [
+        #             datapath.ofproto_parser.OFPActionOutput(out_port),
+        #         ]
 
         # send packet out message for this packet
+
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
+
+        if fdb:
+            actions = [
+                datapath.ofproto_parser.OFPActionOutput(fdb[0][1]),
+            ]
+        else:
+            actions = [
+                datapath.ofproto_parser.OFPActionOutput(ofproto.OFPP_FLOOD),
+            ]
+
         out = datapath.ofproto_parser.OFPPacketOut(
             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
             actions=actions, data=data)
