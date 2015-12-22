365eb0f279e39be773fbcbc721a9b02352d460f0
Keichi Takahashi <keichi.t@me.com>
2015-12-22 21:37:36 +0900

    Fix bad output port error (again)

--- sdnmpi/router.py
+++ sdnmpi/router.py
@@ -79,17 +79,35 @@ class Router(app_manager.RyuApp):
             if dp.id in self.dps:
                 del self.dps[dp.id]
 
+    def _add_flows_for_path(self, fdb, src, dst):
+        for (dpid, out_port) in fdb:
+            if dpid in self.dps:
+                datapath = self.dps[dpid]
+                self._add_flow(datapath, src, dst, out_port)
+
+    def _send_packet_out(self, fdb, datapath, data):
+        ofproto = datapath.ofproto
+        ofproto_parser = datapath.ofproto_parser
+
+        for (dpid, out_port) in fdb:
+            if datapath.id == dpid:
+                # TODO Use buffer if available
+                actions = [ofproto_parser.OFPActionOutput(out_port)]
+                out = ofproto_parser.OFPPacketOut(
+                    datapath=datapath, in_port=ofproto.OFPP_NONE,
+                    actions=actions, buffer_id=ofproto.OFP_NO_BUFFER,
+                    data=data)
+                datapath.send_msg(out)
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        ofproto = datapath.ofproto
-        ofproto_parser = datapath.ofproto_parser
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
-        dst = eth.dst
         src = eth.src
+        dst = eth.dst
 
         # ignore LLDP packet
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
@@ -100,30 +118,22 @@ class Router(app_manager.RyuApp):
         # ignore IPv6 multicast packets
         if dst.startswith("33:33"):
             return
-
-        dpid = datapath.id
-
-        self.logger.info("Packet in at %s (%s) %s -> %s", dpid, msg.in_port,
-                         src, dst)
-
+        # Handle MPI packets
         if dst.startswith("02:00"):
             return self._mpi_packet_in_handler(ev)
 
+        self.logger.info("Packet in at %s (%s) %s -> %s", datapath.id,
+                         msg.in_port, src, dst)
+
         fdb = self.send_request(FindRouteRequest(src, dst)).fdb
-        # Install rules to all datapaths in path
-        if fdb:
-            for (dpid, out_port) in fdb:
-                if dpid in self.dps:
-                    self._add_flow(self.dps[dpid], src, dst, out_port)
 
         if fdb:
-            actions = [ofproto_parser.OFPActionOutput(fdb[0][1])]
-            out = ofproto_parser.OFPPacketOut(
-                datapath=datapath, in_port=ofproto.OFPP_NONE, actions=actions,
-                buffer_id=ofproto.OFP_NO_BUFFER, data=msg.data)
-            datapath.send_msg(out)
+            # Install rules to all datapaths in path
+            self._add_flows_for_path(fdb, src, dst)
+            # Output packet from current switch
+            self._send_packet_out(fdb, datapath, msg.data)
         else:
-            req = BroadcastRequest(msg.data, dpid, msg.in_port)
+            req = BroadcastRequest(msg.data, datapath.id, msg.in_port)
             self.send_request(req)
 
     def _mpi_packet_in_handler(self, ev):
